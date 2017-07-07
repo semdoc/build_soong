b1f46010720779c6855fc0a9b01d75167797215e
Colin Cross <ccross@android.com>
2017-07-07 14:33:33 -0700

    Don't pass same argument twice for defaults modules initialization
    InitDefaultsModule and InitDefaultableModule were requiring the
    module to be passed in as an argument twice, once as a
    android.Module and once as an android.DefaultableMOdule.  Use
    a type assertion instead.
    
    Test: builds
    Change-Id: I57d721e68a4f016e02c17a678af5f2ba25a9a430

--- cc/cc.go
+++ cc/cc.go
@@ -275,7 +275,7 @@ var (
 // to construct the output file.  Behavior can be customized with a Customizer interface
 type Module struct {
 	android.ModuleBase
-	android.DefaultableModule
+	android.DefaultableModuleBase
 
 	Properties BaseProperties
 	unused     UnusedProperties
@@ -339,7 +339,7 @@ func (c *Module) Init() android.Module {
 
 	android.InitAndroidArchModule(c, c.hod, c.multilib)
 
-	android.InitDefaultableModule(c, c)
+	android.InitDefaultableModule(c)
 
 	return c
 }
@@ -1155,7 +1155,7 @@ func (c *Module) HostToolPath() android.OptionalPath {
 //
 type Defaults struct {
 	android.ModuleBase
-	android.DefaultsModule
+	android.DefaultsModuleBase
 }
 
 func (*Defaults) GenerateAndroidBuildActions(ctx android.ModuleContext) {
@@ -1192,7 +1192,7 @@ func DefaultsFactory(props ...interface{}) android.Module {
 		&VndkProperties{},
 	)
 
-	android.InitDefaultsModule(module, module)
+	android.InitDefaultsModule(module)
 
 	return module
 }
